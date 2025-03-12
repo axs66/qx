c0c41862675b3f6fc288849de090a6f620a45f1b
A先生 <142523398+axs66@users.noreply.github.com>
2025-03-12 18:39:13 +0800

    Update mkqqyy.js

--- mkqqyy.js
+++ mkqqyy.js
@@ -1,11 +1,3 @@
-//mikoto - 修改点歌卡片的封面
-//使用方法：圈x 添加主机名  api.dragonlongzhu.cn
-//修改本配置的文件名为mkqqyy.js
-//把这个配置放到文件app-quantumultx-Scripts
-//修改本配置显示文本和封面链接
-//然后点歌
-
-
 let body = $response.body;
 if (body) {
   try {
@@ -17,9 +9,9 @@ if (body) {
     
       obj.data.song_name = originalName + "-" + originalSinger;
       // 将歌手改为固定文本“点击播放—>”
-      obj.data.song_singer = "W先生定制电台>>>";
+      obj.data.song_singer = "Axs电台   播放>>>";
       // 修改封面为指定链接
-      obj.data.cover = "http://fmc-75014.picgzc.qpic.cn/consult_viewer_pic__cff92031-5429-42d4-a3fe-5ddcd20b13b6_1741445606820.jpg";
+      obj.data.cover = "http://q4.qlogo.cn/headimg_dl?dst_uin=1317208960&spec=640";
     }
     $done({body: JSON.stringify(obj)});
   } catch (e) {
