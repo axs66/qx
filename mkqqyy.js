873b1e9b6adb1ec6b5777fd22dd1842c22accc73
A先生 <142523398+axs66@users.noreply.github.com>
2025-03-13 14:10:13 +0800

    Update mkqqyy.js

--- mkqqyy.js
+++ mkqqyy.js
@@ -7,9 +7,9 @@ if (body) {
       let originalName = obj.data.song_name || "";
       let originalSinger = obj.data.song_singer || "";
     
-      obj.data.song_name = originalName + "-" + originalSinger;
+      obj.data.song_name = "恭喜发财"+ ","+ "大吉大利";
       // 将歌手改为固定文本“点击播放—>”
-      obj.data.song_singer = "Axs电台   播放>>>";
+      obj.data.song_singer = "恭喜发财，大吉大利";
       // 修改封面为指定链接
       obj.data.cover = "http://q4.qlogo.cn/headimg_dl?dst_uin=1107621373&spec=640";
     }
