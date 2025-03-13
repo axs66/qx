14ac555321ec3a9b1cc57deb384e5dafb7346152
A先生 <142523398+axs66@users.noreply.github.com>
2025-03-13 14:10:54 +0800

    Update mkqqyy.js

--- mkqqyy.js
+++ mkqqyy.js
@@ -9,7 +9,7 @@ if (body) {
     
       obj.data.song_name = "恭喜发财"+ ","+ "大吉大利";
       // 将歌手改为固定文本“点击播放—>”
-      obj.data.song_singer = "恭喜发财，大吉大利";
+      obj.data.song_singer = "ㅤㅤㅤㅤㅤㅤㅤㅤ";
       // 修改封面为指定链接
       obj.data.cover = "http://q4.qlogo.cn/headimg_dl?dst_uin=1107621373&spec=640";
     }
