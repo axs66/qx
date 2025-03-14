e7f3987850d280f42ec3e65c4a20ad9c42b698c7
A先生 <142523398+axs66@users.noreply.github.com>
2025-03-14 21:54:01 +0800

    Update hbdg.js

--- hbdg.js
+++ hbdg.js
@@ -1,31 +1,32 @@
-const customTitle = "恭喜发财，大吉大利";
-const customArtist = "ㅤㅤㅤㅤㅤㅤㅤㅤ";
-const customCover = `http://fmc-75014.picgzc.qpic.cn/consult_viewer_pic__16e6b048-a625-481f-ac4f-174642e8051f_1741840724601.jpg?param=130y130&_ts=${Date.now()}`;
-
 let body = $response.body;
+let headers = $response.headers;
 
-try {
+if (body) {
+  try {
     let obj = JSON.parse(body);
 
-    if (obj.data) {
-        // 强制覆盖封面字段
-        obj.data.cover = customCover;
-        obj.data.picUrl = customCover;
-        if (obj.data.album) {
-            obj.data.album.picUrl = customCover;
-        }
+    if (obj && obj.data) {
+      const songCombination = {
+        song_name: "恭喜发财,大吉大利！",
+        song_singer: "领取红包",
+        cover: "http://fmc-75014.picgzc.qpic.cn/consult_viewer_pic__934190a1-8b9f-4eea-b8ee-04fd82211d31_1741878745602.jpg"
+      };
 
-        // 修改其他信息
-        obj.data.songname = customTitle;
-        obj.data.name = customArtist;
+      obj.data.songname = songCombination.song_name;
+      obj.data.name = songCombination.song_singer;
+      obj.data.cover = songCombination.cover;
 
-        // 调试日志
-        console.log(`封面已强制替换：${customCover}`);
-        console.log(`完整响应：${JSON.stringify(obj, null, 2)}`);
+      let updatedBody = JSON.stringify(obj);
+      $done({ body: updatedBody, headers: headers });
+
+    } else {
+      $done({ body: body, headers: headers });
     }
 
-    $done({ body: JSON.stringify(obj) });
-} catch (e) {
-    console.log("JSON 解析失败:", e);
-    $done({});
+  } catch (e) {
+    console.log("by 贾队长:", e);
+    $done({ body: body, headers: headers });
+  }
+} else {
+  $done({});
 }
