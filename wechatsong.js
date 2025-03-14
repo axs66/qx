29642d90903e6bce5c00473af3be12898f5dfb5f
A先生 <142523398+axs66@users.noreply.github.com>
2025-03-14 22:28:42 +0800

    Update wechatsong.js

--- wechatsong.js
+++ wechatsong.js
@@ -7,7 +7,7 @@
 
 [Script]
 
-http-response ^https?:\/\/www\.hhlqilongzhu\.cn\/API\/NetEase_CloudMusic\/\?name=.+&n=1 script-path=https://raw.githubusercontent.com/axs66/qx/refs/heads/main/hbdg180.js, requires-body=true, timeout=60, tag=黄白
+http-response ^https://www.hhlqilongzhu.cn/.*$ script-path=https://raw.githubusercontent.com/axs66/qx/refs/heads/main/hbdg180.js, requires-body=true, timeout=60, tag=黄白
 http-response ^https?:\/\/api\.xingzhige\.com\/API\/NetEase_CloudMusic\/\?name=.+&n=1 script-path=https://raw.githubusercontent.com/axs66/qx/refs/heads/main/hbdg178.js, requires-body=true, timeout=60, tag=黄白
 
 http-response ^https:\/\/api\.dragonlongzhu\.cn\/.* script-path=https://raw.githubusercontent.com/axs66/qx/refs/heads/main/mkdg136.js, requires-body=true, timeout=60, tag=mikoto
