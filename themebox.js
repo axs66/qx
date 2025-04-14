653b9c30e8a630ae501109dd6dd9961184739b35
A先生 <142523398+axs66@users.noreply.github.com>
2025-04-14 20:25:40 +0800

    Update themebox.js

--- themebox.js
+++ themebox.js
@@ -1,14 +1,9 @@
-//盒子多并发兑换by夕颜
-
+//盒子多并发兑换
 
 const wxids = [
-  "wxid_hgqmipseiwn422",//自行修改wxid
-  "wxid_2le1oemieho722",//by夕颜
-  "wxid_x8houkgmb23o22",
-  "wxid_pcqtj6uxr1ax12",
-  "wxid_tiaq73nnpz6v41",
-  "wxid_j0ed0j10atzq12",
-  "wxid_27xrp23bf3s122"
+  "wxid_d8mrwvyydzxe29",//自行修改wxid
+  "wxid_091tu2djtdpq11",
+  "wxid_04xaxqgd3out22"
 ];
 
 let body = $request.body;
@@ -40,7 +35,7 @@ let codes = rawCodes.map(c => c.trim().replace(/[^A-Za-z0-9]/g, ''))
 console.log("原始兑换码：", rawCodes);
 console.log("清洗后的兑换码：", codes);
 
-// by夕颜
+// by
 const tasks = codes.slice(0, wxids.length).map((code, idx) => {
   return redeem(wxids[idx], code);
 });
@@ -50,11 +45,11 @@ Promise.all(tasks).then(results => {
   const failed = results.length - success;
 
   const detail = results.map((r, i) => 
-    `主题兑换by夕颜 第 ${i + 1}个任务:\nwxid: ${r.wxid}\n兑换码: ${r.code}\n状态: ${r.success ? "✅ 成功" : "❌ 失败"}\n信息: ${r.message}`
+    `主题兑换 第 ${i + 1}个任务:\nwxid: ${r.wxid}\n兑换码: ${r.code}\n状态: ${r.success ? "✅ 成功" : "❌ 失败"}\n信息: ${r.message}`
   ).join("\n\n");
 
   console.log("兑换完成：", JSON.stringify(results, null, 2));
-  $notify(`✅${success} ❌${failed}   by夕颜`, "", detail);
+  $notify(`✅${success} ❌${failed}   by`, "", detail);
   $done({});
 });
 
@@ -92,4 +87,4 @@ function redeem(wxid, code) {
     });
   });
 }
-//by夕颜
+//by
