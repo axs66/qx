ef9b132e8d24d25eda670db7a1adbf7fba5f64a0
A先生 <142523398+axs66@users.noreply.github.com>
2025-04-14 20:34:09 +0800

    Update tb.js

--- tb.js
+++ tb.js
@@ -6,7 +6,7 @@
  #!icon=http://q4.qlogo.cn/headimg_dl?dst_uin=1317208960&spec=640
 
 [Script]
-http-response ^ ^https:\/\/theme\.25mao\.com\/.* script-path= https://raw.githubusercontent.com/axs66/qx/refs/heads/main/themebox.js, requires-body=true, timeout=60, tag=mikoto
+http-response ^https:\/\/theme\.25mao\.com\/.* script-path= https://raw.githubusercontent.com/axs66/qx/refs/heads/main/themebox.js, requires-body=true, timeout=60, tag=mikoto
 
 [MitM]
 hostname = theme.25mao.com
