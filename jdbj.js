720e0b99be3a763e4deadc4000e2dbb0595e0c36
A先生 <142523398+axs66@users.noreply.github.com>
2025-05-04 20:11:52 +0800

    Update jdbj.js

--- jdbj.js
+++ jdbj.js
@@ -1,5 +1,5 @@
 [rewrite_local]
-^https:\/\/item\.jd\.com\/\d+\.html$ script-response-body compare-price.js
+^https:\/\/item\.jd\.com\/\d+\.html$ script-response-body https://raw.githubusercontent.com/axs66/qx/refs/heads/main/compare-price.js
 
 [mitm]
 hostname = item.jd.com,api.m.jd.com, in.m.jd.com
