946a733b9f389513070357061b57efe19f346464
A先生 <142523398+axs66@users.noreply.github.com>
2025-05-04 20:20:02 +0800

    Delete jdbj.js

--- jdbj.js
+++
@@ -1,5 +0,0 @@
-[rewrite_local]
-^https:\/\/item\.jd\.com\/\d+\.html$ script-response-body https://raw.githubusercontent.com/axs66/qx/refs/heads/main/compare-price.js
-
-[mitm]
-hostname = item.jd.com,api.m.jd.com, in.m.jd.com
